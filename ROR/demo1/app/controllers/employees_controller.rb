b3b46a10e496e3bfec086d0272a2b2cdd49d606c
madhu-patidar <madhusudan.patidar@neosofttech.com>
2016-11-24 21:24:14 +0530

    add demo application

---
+++ ROR/demo1/app/controllers/employees_controller.rb
@@ -0,0 +1,40 @@
+class EmployeesController < ApplicationController
+  
+  def index
+    @employees = Employee.all
+    
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+   
+    if @employee.update(params.require(:employee).permit(:name, :designation,:salary))
+      redirect_to @employee
+    else
+      render 'edit'
+    end
+  end
+  def create
+    @employee = Employee.new(params.require(:employee).permit(:name, :designation,:salary))
+    @employee.save
+    redirect_to @employee
+  end
+
+   def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    redirect_to employees_path
+  end
+
+end
